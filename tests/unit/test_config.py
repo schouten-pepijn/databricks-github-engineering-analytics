0fa2d5609dd975848a48efbb77c04af0ccee6d65
Pepijn Schouten <pepijn.sch@gmail.com>
2026-09-19 00:37:05 +0200

    test: update config tests (tests)

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,17 +4,8 @@ from github_engineering_analytics.common.config import PipelineConfig
 def test_pipeline_config_builds_table_names():
     config = PipelineConfig(catalog="main")
 
-    assert (
-        config.bronze_issues_table
-        == "main.github_analytics_bronze.github_issues_raw"
-    )
+    assert config.bronze_issues_table == "main.github_analytics_bronze.github_issues_raw"
 
-    assert (
-        config.silver_issues_table
-        == "main.github_analytics_silver.github_issues"
-    )
+    assert config.silver_issues_table == "main.github_analytics_silver.github_issues"
 
-    assert (
-        config.watermark_table
-        == "main.github_analytics_control.ingestion_watermark"
-    )
+    assert config.watermark_table == "main.github_analytics_control.ingestion_watermark"
